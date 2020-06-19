6bdd71106a3017e60721527e8d77c32a81e254c4
Vinicius Teobaldo <viniciusr42@gmail.com>
2020-06-18 22:11:23 -0300

    Desafio Cálculo de aposentadoria

---
+++ Desafios/aposentadoria.js
@@ -0,0 +1,15 @@
+const nome = 'Vinicius'
+const sexo = 'M'
+const idade = 50
+const contribuicao = 30
+
+const calculoContribuicao = idade + contribuicao
+const homemPodeAposentar = sexo == 'M' && contribuicao >= 35 && calculoContribuicao >= 95
+const mulherPodeAposentar = sexo == 'F' && contribuicao >= 30 && calculoContribuicao >= 85
+
+
+if ( homemPodeAposentar || mulherPodeAposentar) {
+    console.log(`${nome}, você pode se aposentar!`)
+} else {
+    console.log(`${nome}, você não pode se aposentar!`)
+}
